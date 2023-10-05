b04c5c29ffe5b9c13bee8669081c9a26960649f5
Lucas colman <lucaspaganelli20@gmail.com>
2023-10-05 16:03:10 -0300

    cors

--- app.js
+++ app.js
@@ -6,21 +6,22 @@ const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express();
 app.use(express.json());
-  app.use(cors({
-    origin:(origin,callback)=>{
-        const ACCEPTED_ORIGINS=[
-            'http://localhost:8080',
-            'http://localhost:1234'
-        ]
-        if (ACCEPTED_ORIGINS.includes(origin)) {
-            return callback(null, true)
-          }
-          if (!origin) {
-            return callback(null, true)
-          }
-          return callback(new Error('Not allowed by CORS'))      
-    }
-  }));
+app.use(cors());
+//   app.use(cors({
+//     origin:(origin,callback)=>{
+//         const ACCEPTED_ORIGINS=[
+//             'http://localhost:8080',
+//             'http://localhost:1234'
+//         ]
+//         if (ACCEPTED_ORIGINS.includes(origin)) {
+//             return callback(null, true)
+//           }
+//           if (!origin) {
+//             return callback(null, true)
+//           }
+//           return callback(new Error('Not allowed by CORS'))      
+//     }
+//   }));
 app.disable('x-powered-by');
 
 
